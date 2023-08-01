a408384a647e1b36f26ff12913e04588c82cdfd3
MK <01aldrn10@gmail.com>
2023-08-01 21:25:07 +0900

    s

--- Typing_Game/typing.js
+++ Typing_Game/typing.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded',function(){
-  
+
+  // 클릭하면 이동
+  const startBtn = document.querySelector('.click');
+  startBtn.addEventListener('click',function(){
+    const startscreen  = document.querySelector('header');
+    startBtn.classList.add('hidden')
+    startscreen.style.height='200px'
+  })
 })
