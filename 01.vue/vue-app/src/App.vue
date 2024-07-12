25439c33d10345a0284e53ff5db49041d7ab4220
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-07-12 10:16:24 +0900

    s

--- 01.vue/vue-app/src/App.vue
+++ 01.vue/vue-app/src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import HeaderDisplay from './components/HeaderDisplay.vue'
+import HeaderDisplay from './components/weather/HeaderDisplay.vue'
 import WeatherContent from './components/weather/WeatherContent.vue'
 
 export default {
