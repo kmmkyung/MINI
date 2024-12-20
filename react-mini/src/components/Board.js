867ec2c35c14230dd8eb38ae6d7e7fc58093d029
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-12-21 00:32:02 +0900

    s

--- react-mini/src/components/Board.js
+++ react-mini/src/components/Board.js
@@ -5,8 +5,7 @@ import '../css/Board.css'
 function Board(){
   // state
   let [squares, setSquares] = useState(Array(9).slice())
-  let [xIsNext,setXIsNext] = useState(true)
-  let status = `Next Player ${xIsNext?'X':'O'}`
+  let [xIsNext, setXIsNext] = useState(true)
 
   // function
   function renderSquare(i){
@@ -14,10 +13,31 @@ function Board(){
   }
 
   function handleClick(i){
-    const squaresArr = squares.slice();
+    const squaresArr = squares.slice();    
+    if(calculateWinner(squaresArr) || squaresArr[i]){
+      return;
+    }
     squaresArr[i] = xIsNext? 'X':'O';
     setSquares(squaresArr)
     setXIsNext(previousState => !previousState)
+  }
+
+  function calculateWinner(squares){
+    const lines = [[0,1,2], [3,4,5], [6,7,8], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    for (let i = 0; i < lines.length; i++){
+      const [a,b,c] = lines[i]
+      if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){ return squares[a] }
+    }
+    return null;
+  }
+
+  let winner = calculateWinner(squares)
+  let status
+  if(winner){
+    status = `Winner: ${winner}`
+  }
+  else{
+    status = `Next player: ${xIsNext? 'X' : 'O'}`
   }
 
   return (
