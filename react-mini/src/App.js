7399470272279afb253becee60a4a2d083467945
Min Kyung <119033846+kmmkyung@users.noreply.github.com>
2024-12-22 01:35:51 +0900

    s

--- react-mini/src/App.js
+++ react-mini/src/App.js
@@ -1,14 +1,57 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 
 function App() {
+  //state
+  let [history, setHistory] = useState([{squares: Array(9).fill(null)}])
+  let [xIsNext, setXIsNext] = useState(true)
+  let current = history[history.length-1] // 마지막 누른 것
+
+  function handleClick(i){
+    const squaresArr = current.squares.slice();        
+    if(calculateWinner(squaresArr) || squaresArr[i]){
+      return;
+    }
+    squaresArr[i] = xIsNext? 'X':'O';
+    setHistory([...history,{squares: squaresArr}])
+    setXIsNext(previousState => !previousState)
+  }
+
+  function calculateWinner(squares){
+    const lines = [[0,1,2], [3,4,5], [6,7,8], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    for (let i = 0; i < lines.length; i++){
+      const [a,b,c] = lines[i]
+      if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){ return squares[a] }
+    }
+    return null;
+  }
+  let winner = calculateWinner(current.squares)
+  let status
+  if(winner){
+    status = `Winner: ${winner}`
+  }
+  else{
+    status = `Next player: ${xIsNext? 'X' : 'O'}`
+  }
+
+  const moves = history.map(function(ele,idx){
+    const desc = idx? `Go to move #${idx}` : 'Go to game start';
+    return <li key={idx}><button>{desc}</button></li>
+  })
+
+  
+
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board></Board>
+        <Board squares={current.squares} handleClick={handleClick}></Board>
       </div>
       <div className='game-info'>
-
+        <div className='status'>{status}</div>
+        <ol>
+          {moves}
+        </ol>
       </div>
     </div>
   );
--- react-mini/src/components/Board.js
+++ react-mini/src/components/Board.js
@@ -1,48 +1,15 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import Square from './Square';
 import '../css/Board.css'
 
-function Board(){
-  // state
-  let [squares, setSquares] = useState(Array(9).slice())
-  let [xIsNext, setXIsNext] = useState(true)
-
+function Board(props){
   // function
   function renderSquare(i){
-    return <Square value={squares[i]} handleClick={()=>handleClick(i)}></Square>
-  }
-
-  function handleClick(i){
-    const squaresArr = squares.slice();    
-    if(calculateWinner(squaresArr) || squaresArr[i]){
-      return;
-    }
-    squaresArr[i] = xIsNext? 'X':'O';
-    setSquares(squaresArr)
-    setXIsNext(previousState => !previousState)
-  }
-
-  function calculateWinner(squares){
-    const lines = [[0,1,2], [3,4,5], [6,7,8], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-    for (let i = 0; i < lines.length; i++){
-      const [a,b,c] = lines[i]
-      if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){ return squares[a] }
-    }
-    return null;
-  }
-
-  let winner = calculateWinner(squares)
-  let status
-  if(winner){
-    status = `Winner: ${winner}`
-  }
-  else{
-    status = `Next player: ${xIsNext? 'X' : 'O'}`
+    return <Square value={props.squares[i]} handleClick={()=>props.handleClick(i)}></Square>
   }
 
   return (
     <div>
-      <div className='status'>{status}</div>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
